bc4ccff490c8f71153ecfd7a5a7374680c487507
Laci444 <szilagyilaci44@gmail.com>
2025-02-27 10:22:26 +0100

    refactor: use Lombok in Nationality

--- src/main/java/nationalize/NationalizeClient.java
+++ src/main/java/nationalize/NationalizeClient.java
@@ -9,15 +9,16 @@ import feign.gson.GsonDecoder;
 
 public interface NationalizeClient {
 
-    @RequestLine("GET /?name={name}")
-    Nationality getNationality(@Param("name") String name);
+  @RequestLine("GET /?name={name}")
+  Nationality getNationality(@Param("name") String name);
 
-    static NationalizeClient newInstance() {
-        return Feign.builder()
-                .decoder(new GsonDecoder(new GsonBuilder()
-                        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-                        .create()))
-                .target(NationalizeClient.class, "https://api.nationalize.io/");
-    }
-
+  static NationalizeClient newInstance() {
+    return Feign.builder()
+        .decoder(new GsonDecoder(
+            new GsonBuilder()
+                .setFieldNamingPolicy(
+                    FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create()))
+        .target(NationalizeClient.class, "https://api.nationalize.io/");
+  }
 }
